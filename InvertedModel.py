2e11a97839308f65c8e411741c62c10580e69a58
cedricIGV <cedrib@umich.edu>
2018-04-11 16:27:35 -0400

    merged

--- InvertedModel.py
+++ InvertedModel.py
@@ -5,7 +5,7 @@ import keras
 import numpy as np
 from PIL import Image
 from keras.models import Sequential
-from keras.layers import Conv1D, MaxPooling1D, Activation, Dropout, Flatten, Dense, Reshape
+from keras.layers import Conv1D, GlobalMaxPooling2D, Activation, Dropout, Flatten, Dense, Reshape
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 
 
@@ -34,7 +34,7 @@ model.add(Reshape((150,150)))
 
 model.add(Conv1D(64, 3))
 model.add(Activation('relu'))
-model.add(MaxPooling1D(pool_size=2))
+model.add(GlobalMaxPooling2D())
 
 #model.add(Conv2D(150, (3,3)))
 #model.add(Activation('relu'))
