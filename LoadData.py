4c894eec52d977ef465c709406cc7a98634cb566
cedricIGV <cedrib@umich.edu>
2018-04-15 11:18:52 -0400

    predict

--- LoadData.py
+++ LoadData.py
@@ -1,6 +1,7 @@
 import os
 import glob
 import keras
+import numpy as np
 from PIL import Image
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Activation, Dropout, Flatten, Dense
@@ -45,8 +46,23 @@ def main():
     model.add(Activation('sigmoid'))
 
     model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
-    model.fit_generator(train_generator, steps_per_epoch = 2000, epochs=25, verbose=2)
+    model.fit_generator(train_generator, steps_per_epoch = 20, epochs=5, verbose=2)
     model.save_weights('classifier.h5')
+
+    path = os.path.join(os.getcwd(), "dog")
+    filenames = os.listdir(path)
+    i = 0
+    x = []
+    for filename in filenames:
+        if i == 50:
+            break
+        im = Image.open(os.path.join(path, filename))
+        arr = np.array(im)
+        x.append(arr)
+        i += 1
+
+    out = model.predict(np.array(x))
+    print(out)
 
 if __name__ == "__main__":
     main()
