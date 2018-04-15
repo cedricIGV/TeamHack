1d4128f9e96beed117900bc48a7a68cba92d69dd
cedricIGV <cedrib@umich.edu>
2018-04-15 10:52:46 -0400

    model in color

--- InvertedModel.py
+++ InvertedModel.py
@@ -5,7 +5,7 @@ import keras
 import numpy as np
 from PIL import Image
 from keras.models import Sequential
-from keras.layers import Conv1D, GlobalMaxPooling1D, Activation, Dropout, Flatten, Dense, Reshape
+from keras.layers import Conv1D, Conv2D, GlobalMaxPooling2D, Activation, Dropout, Flatten, Dense, Reshape
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 import sys
 
@@ -17,19 +17,19 @@ def main():
     i = 1
     for filename in os.listdir(os.path.join(os.getcwd(), "dog")):
         path = os.path.join(os.getcwd(), "dog", filename)
-        y.append(img_to_array(Image.open(path).convert('L')))
+        y.append(img_to_array(Image.open(path)))
         x.append([1,0])
-        if i % int(total/100) == 0:
-            print("Loading Images " + str(int((i*50)/total)) + "%", end='\r'),
-        i += 1
+        #if i % int(total/100) == 0:
+        #    print("Loading Images " + str(int((i*50)/total)) + "%", end='\r'),
+        #i += 1
 
     for filename in os.listdir(os.path.join(os.getcwd(), "cat")):
         path = os.path.join(os.getcwd(), "cat", filename)
-        y.append(img_to_array(Image.open(path).convert('L')))
+        y.append(img_to_array(Image.open(path)))
         x.append([0,1])
-        if i % int(total/100) == 0:
-            print("Loading Images " + str(int((i*50)/total)) + "%", end='\r'),
-        i += 1
+        #if i % int(total/100) == 0:
+        #    print("Loading Images " + str(int((i*50)/total)) + "%", end='\r'),
+        #i += 1
         
     print("Image Loading Complete")
     print("Compiling Model...")
@@ -38,18 +38,22 @@ def main():
     model.add(Activation('sigmoid'))
     model.add(Dense(640, activation='relu'))
     model.add(Dense(22500, activation='relu'))
-    model.add(Reshape((150,150)))
+    model.add(Reshape((150,75,2)))
 
-    model.add(Conv1D(150, 3))
+    model.add(Conv2D(150, (3,3)))
     model.add(Activation('relu'))
-    model.add(GlobalMaxPooling1D())
+    model.add(GlobalMaxPooling2D())
 
     #model.add(Conv1D(150, 3))
     #model.add(Activation('relu'))
     #model.add(GlobalMaxPooling1D())
-
+    
+    model.add(Dense(100, activation = 'tanh'))
+    model.add(Dense(1000, activation = 'sigmoid'))
+    model.add(Dense(10000, activation = 'tanh'))
     model.add(Dense(22500, activation='relu'))
-    model.add(Reshape((150,150)))
+    model.add(Dense(22500*3, activation='sigmoid'))
+    model.add(Reshape((150,150,3)))
 
     model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
     print("Compiling Model Complete")
@@ -64,6 +68,14 @@ def main():
         i = i+100
         print("Training Model " + str(int((i*100)/len(x))) + "%", end='\r'),
 
+    x = []
+    x.append([1,0])
+    x = np.array(x)
+    x = np.reshape(x, (1,1,2))
+    arr = model.predict(x)
+    img = Image.fromarray(arr[0], 'RGB')
+    img.save('out.bmp')
     print("Network Saved!")
+
 if __name__ == "__main__":
     main()
